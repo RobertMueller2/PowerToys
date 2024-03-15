bceded3e3cf867c5bfd0e4c18752ea94225aeaae
Rene D. Obermueller <cmdrrdo@gmail.com>
2024-03-15 16:06:10 +0100

    add BoilerplateText plugin for Powertoys Run

--- src/codeAnalysis/GlobalSuppressions.cs
+++ src/codeAnalysis/GlobalSuppressions.cs
@@ -62,3 +62,5 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "<Dotnet port with style preservation>", Scope = "namespaceanddescendants", Target = "MouseWithoutBorders")]
 [assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names should not contain underscore", Justification = "<Dotnet port with style preservation>", Scope = "namespaceanddescendants", Target = "MouseWithoutBorders")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name should match first type name", Justification = "<Dotnet port with style preservation>", Scope = "namespaceanddescendants", Target = "MouseWithoutBorders")]
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "<Temporary exclusion for standalone project>")]
